ac46e4b0750825310dbdff35815aa6ccd4c73aee
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-12 13:27:24 +0100

    Fatto scheletro e create le variabili

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// CONSEGNA 
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+
+
+// SOLUZIONE
+
+// Dichiaro prima tutte le variabili che entrano in gioco
+const numKm;
+const userAge;
+const travelPrice;
+const finalMessage
+
+// Chiedo all'utente il numero di chilometri che vuole percorrere
+
+// Chiedo all'utente l'età del passeggero
+
+// Definisco il prezzo del biglietto
+
+// Applico eventuali sconti
+
+// Output con massimo 2 cifre decimali
